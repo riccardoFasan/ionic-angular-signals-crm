944a61ebca93740367338d915c6f3d0deb7287c2
Riccardo Fasan <riccardo.fasan@justbit.it>
2023-12-19 14:45:17 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'food-diary',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
